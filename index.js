4fef760cfcc374720362cad7eea1b6a0ede9efb5
Andrew Chilton <andychilton@gmail.com>
2018-07-19 13:51:16 +1200

    Switch to using math-random

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const mathRandom = require('math-random')
+
 function yid() {
-  let id = Date.now() + '-' + String(Math.random()).substr(2, 13)
-  while ( id.length < 27 )
+  let id = Date.now() + '-' + String(mathRandom()).substr(2, 13)
+
+  while ( id.length < 27 ) {
     id += '0'
+  }
+
   return id
 }
 
